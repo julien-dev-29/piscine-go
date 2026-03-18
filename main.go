7c79c0edc703e643a77f2a31358bd3f1d817f182
jurol <rollandjulien718@gmail.com>
2026-03-18 10:21:12 +0100

    Finish utltimatedivmod exerice

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"piscine/piscine"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
-	piscine.PrintNbr(-123)
-	piscine.PrintNbr(0)
-	piscine.PrintNbr(123)
-	z01.PrintRune('\n')
+	a := 13
+	b := 2
+	piscine.UltimateDivMod(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
 }
